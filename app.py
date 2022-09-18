efb6de86a0352b75a1be48c8938cff59b0c1f001
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2022-09-18 09:56:50 +0200

    files created

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("login.html")
+
+app.run(host="0.0.0.0", port = 5001)
