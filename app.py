5f1ad2ecd0bbc1e05a2f19ab36d56d97759d5d8a
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2022-10-01 19:48:18 +0200

    base functions

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 #!/bin/python
 
-from flask import Flask, render_template
+import re
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def index():
-	return render_template("login.html")
+	return render_template("home.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    #work to do
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin":
+            error = "Invalid credentials. Please try again."
+        else:
+            return redirect("home")
+    return render_template("login.html")
 
 app.run(host="0.0.0.0", port = 5001)
