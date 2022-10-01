8ba1b87c9c100afe4a158d18f9e5aaca75b6e6e2
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2022-10-01 20:00:02 +0200

    added hash and verify functions

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 
 import re
 from flask import Flask, render_template, redirect, url_for, request
+import base64
+import hashlib
+import secrets
+
+ALGORITHM = "pbkdf2_sha256"
+ITERATIONS = 26000
 
 
 app = Flask(__name__)
@@ -22,3 +28,17 @@ def login():
     return render_template("login.html")
 
 app.run(host="0.0.0.0", port = 5001)
+
+def hash_password(password, salt=None): #function to hash the passwords
+    if salt is None:
+        salt = secrets.token_hex(16)    #get a random string in hex
+    
+    pw_hash = hashlib.pbkdf2_hmac(  #using hmac as hashing algorithm
+        "sha256", password.encode("utf-8"), salt.encode("utf-8"), ITERATIONS
+    )
+    b64_hash = base64.b64encode(pw_hash).decode("ascii").strip()    #encode it to base64
+    return "{}${}".format(salt, b64_hash)  #return salt and password
+
+def verify_password(password, password_hash, salt):
+    compare_hash = hash_password(password, salt)    #hash the password with the function above
+    return secrets.compare_digest(password_hash, compare_hash)  #true or false?
