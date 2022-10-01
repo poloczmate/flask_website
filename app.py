4ea10e9ea964304a3e37cba369841324c3cec220
Mate Polocz <48856366+poloczmate@users.noreply.github.com>
2022-10-01 21:04:30 +0200

    db connection & login site

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
 #!/bin/python
-
 import re
 from flask import Flask, render_template, redirect, url_for, request
 import base64
 import hashlib
 import secrets
+import mysql.connector
 
-ALGORITHM = "pbkdf2_sha256"
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    username = 'root',
+    password = 'qwerasdf00',
+    database = 'flask_website'
+)
+
+cursor = mydb.cursor()
+
+register_sql = "insert into Users (username, salt, password) values (%s, %s, %s);"
+verify_sql = "select PASSWORD, SALT from Users where USERNAME =%s;"
 ITERATIONS = 26000
-
-
-app = Flask(__name__)
-
-@app.route("/home")
-def index():
-	return render_template("home.html")
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    error = None
-    #work to do
-    if request.method == "POST":
-        if request.form["username"] != "admin" or request.form["password"] != "admin":
-            error = "Invalid credentials. Please try again."
-        else:
-            return redirect("home")
-    return render_template("login.html")
-
-app.run(host="0.0.0.0", port = 5001)
 
 def hash_password(password, salt=None): #function to hash the passwords
     if salt is None:
@@ -40,5 +30,34 @@ def hash_password(password, salt=None): #function to hash the passwords
     return "{}${}".format(salt, b64_hash)  #return salt and password
 
 def verify_password(password, password_hash, salt):
-    compare_hash = hash_password(password, salt)    #hash the password with the function above
+    compare_salt, compare_hash = hash_password(password,salt).split('$', 1)    #hash the password with the function above
+    print(compare_hash)
+    print(password_hash)
     return secrets.compare_digest(password_hash, compare_hash)  #true or false?
+
+
+app = Flask(__name__)
+
+@app.route("/home")
+def index():
+	return render_template("home.html")
+
+@app.route("/registration", methods=['GET', 'POST'])
+def registration():
+    return True
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    #work to do
+    if request.method == "POST":
+        tmp = (request.form["username"],)
+        cursor.execute(verify_sql,tmp)
+        results = cursor.fetchall()
+        print(results[0])
+        if verify_password(request.form["password"], results[0][0], results[0][1]):
+            return redirect("home")
+        else:
+            print("Invalid credentials. Please try again.")
+    return render_template("login.html")
+
+app.run(host="0.0.0.0", port = 5001)
